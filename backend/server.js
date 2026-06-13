4a437787e50ab4644be68ed4bf667f2897aa5459
Vinayak Gupta <148940938+gptVinu@users.noreply.github.com>
2026-06-13 21:26:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,8 @@ app.use(
   })
 );
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 // ─── Health Check ─────────────────────────────────────────────────────────────
 app.get("/api/v1/health", (req, res) => {
